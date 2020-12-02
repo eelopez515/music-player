5ad04baf522f7d65a7dee3bd19ce11d20a985bab
eelopez515 <eelopez15@gmail.com>
2020-12-02 17:52:11 -0500

    Components, Styling, and State
    Made two components for player and song, and added some styling to it.
    Also added a a state for the current song

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './styles/app.scss'
 import Player from './components/Player'
 import Song from './components/Song'
+import data from './data.js'
 
 function App() {
+  //State
+  const [songs, setSongs] = useState(data())
+  const [currentSong, serCurrentSong] = useState(songs[1])
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlay, faAngleLeft, faAngleRight, faAlignLeft } from '@fortawesome/free-solid-svg-icons'
+import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
 const Player = () => {
     return (
